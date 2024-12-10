bd233fa22159f87ce9f2ce3046dcd68ed4e84b8d
yujiang31 <yujiang.xia.7e6@itb.cat>
2024-12-10 12:01:28 +0100

    cambios24

--- src/main/kotlin/controllers/ejercicio 1.kt
+++ src/main/kotlin/controllers/ejercicio 1.kt
@@ -3,6 +3,25 @@ package org.example.controllers
 import utilities.abrirScanner
 import utilities.cerrarScanner
 import java.util.*
+
 fun main() {
+    //Llamamos a la funcion de abrir el Scanner
+
+    var scan : Scanner = abrirScanner()
+
+    // Pasamos el escaner
+    var input = pedirnum("Introduce un numero:", scan)
+
+
+
 
 }
+
+
+
+//Función para pedir un número
+fun pedirnum(msg: String, scan: Scanner): Float {
+    print(msg)
+    return scan.nextFloat()
+}
+
