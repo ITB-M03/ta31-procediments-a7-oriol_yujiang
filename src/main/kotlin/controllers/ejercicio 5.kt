f89ab97fb7b5ba89f7fd96af59756d9d8de727c1
yujiang31 <yujiang.xia.7e6@itb.cat>
2024-12-13 13:55:30 +0100

    cambios24dasdasdas

--- src/main/kotlin/controllers/ejercicio 5.kt
+++ src/main/kotlin/controllers/ejercicio 5.kt
@@ -5,15 +5,37 @@ import utilities.cerrarScanner
 import java.util.*
 
 fun main() {
-    // Abrimos el scanner para leer la entrada del usuario
+    // Abrimos el scanner
     val scan: Scanner = abrirScanner()
 
 
-    cerrarScanner(scan)
+    // Escaneo numero
+    var opcion = pedirNumero("Escoge alguna opcion" +
+            "1.- Afegir número (push)\n" +
+            "2.- Treure número (pop)\n" +
+            "3.- Mostrar contingut de la pila\n" +
+            "4.- Sortir",scan)
+
+
+
+    // Crear array vacio num
+
+
+    //Procesar num
+    var proceso = lifo(opcion)
+
+
+
+
+
+
+
 }
 
+
 // Función para solicitar un número al usuario
-fun pedirNumero(msg: String, scan: Scanner): Int {
+
+fun pedirNumero(msg: String, scan: Scanner): Int{
     // Mostramos el mensaje que solicita la entrada del usuario
     print(msg)
 
@@ -24,3 +46,17 @@ fun pedirNumero(msg: String, scan: Scanner): Int {
     return num
 }
 
+
+fun lifo(num1:Int){
+
+    while (num1 != 4){
+
+        when (num1){
+
+            1 ->
+
+
+        }
+    }
+
+}
