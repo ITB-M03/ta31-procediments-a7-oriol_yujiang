82635a6742bb6c6c7734f79958fbf22b37c2d063
yujiang31 <yujiang.xia.7e6@gmail.com>
2024-12-13 23:46:20 +0100

    fsdfse

--- src/main/kotlin/controllers/ejercicio 5.kt
+++ src/main/kotlin/controllers/ejercicio 5.kt
@@ -10,27 +10,27 @@ fun main() {
 
 
     // Escaneo numero
-    var opcion = pedirNumero("Escoge alguna opcion" +
-            "1.- Afegir número (push)\n" +
-            "2.- Treure número (pop)\n" +
-            "3.- Mostrar contingut de la pila\n" +
-            "4.- Sortir",scan)
+    var opcion = pedirNumero("""Escoge alguna opcion
+     1.- Afegir número
+     2.- Treure número
+     3.- Mostrar contingut de la pila
+     4.- Sortir""",
+        scan)
 
 
+    // Creacion de la pila
 
-    // Crear array vacio num
-
+    var content = pila()
 
     //Procesar num
-    var proceso = lifo(opcion)
-
-
+    var proceso = lifo(opcion,scan)
 
 
 
 
 
 }
+
 
 
 // Función para solicitar un número al usuario
@@ -46,17 +46,66 @@ fun pedirNumero(msg: String, scan: Scanner): Int{
     return num
 }
 
+//Creacion de Pila(lista vacia, empty)
 
-fun lifo(num1:Int){
+fun pila():MutableList<Int>{
+
+    var lista = MutableList(10){0}
+    return lista
+}
+
+
+
+
+// Proceso de los numeros
+
+fun lifo(num1:Int, scan:Scanner){
 
     while (num1 != 4){
 
         when (num1){
 
-            1 ->
+            1 -> op1("Introduce un numero para anyadir: ", scan)
+            2 -> op2("Introduce un numero para quitar: ", scan)
+            3 -> op3()
 
 
         }
     }
+
+}
+
+// Opcion 1
+
+fun op1(msg: String, scan: Scanner){
+    // Mostramos el mensaje que solicita la entrada del usuario
+    print(msg)
+
+    // Leemos el número ingresado por el usuario
+    val num: Int = scan.nextInt()
+
+    op3()
+
+
+
+}
+
+
+// Opcion 2
+
+fun op2(msg: String, scan: Scanner){
+    // Mostramos el mensaje que solicita la entrada del usuario
+    print(msg)
+
+    // Leemos el número ingresado por el usuario
+    val num: Int = scan.nextInt()
+
+}
+
+
+// Opcion 3
+
+fun op3(){
+    println(pila())
 
 }
